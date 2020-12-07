d648ddbc5ab38cd8a998e81b282d6990abf6ad22
Todd DiFronzo <fonze@aol.com>
2020-12-06 19:35:27 -0800

    updated

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -59,26 +59,43 @@ class CPU:
     def HLT(self):
         self.running = False
 
+    def ADD(self):
+        operand_a = self.ram[self.pc + 1]
+        operand_b = self.ram[self.pc + 2]
+        self.alu("ADD", operand_a, operand_b)
+        self.pc += 3
+
+    def MUL(self):
+        operand_a = self.ram[self.pc + 1]
+        operand_b = self.ram[self.pc + 2]
+        self.alu('MULTIPLY', operand_a, operand_b)
+        self.pc += 3
+
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        if len(sys.argv) != 2:
+            print('usage: cpu.py filename')
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    comment_split = line.split('#')
+                    code_snip = comment_split[0].strip()
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    if code_snip == '':
+                        continue
+                    
+                    binary = int(code_snip, 2)
+                    self.ram[address] = binary
+                    address += 1
+
+        except FileNotFoundError:
+            print(f'{sys.argv[1]} could not be found')
+            sys.exit(2)
 
 
     def alu(self, op, reg_a, reg_b):
